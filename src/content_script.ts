6a7eb15bc09e8ff66334d15b6fe7da2447e353a3
aoisupersix <aoisupersix@gmail.com>
2021-07-03 22:49:21 +0900

    [add] eslint and prettier

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,13 +1,17 @@
 import $ from 'jquery'
 
-const txQuery = location.search.substring(1).split('&')
-    .map(val => ({ name: val.split('=')[0], value: val.split('=')[1] }))
-    .find(val => val.name === 'tx')
-
+const txQuery = location.search
+    .substring(1)
+    .split('&')
+    .map((val) => ({ name: val.split('=')[0], value: val.split('=')[1] }))
+    .find((val) => val.name === 'tx')
 
 if (txQuery !== undefined) {
     const trainNo = txQuery.value.split('-')[2]
-    const titleSplitWithBr = $('.inner,.ek-onetrain-title-inner', document).html().split('<br>')
-    $('.inner,.ek-onetrain-title-inner', document).html(`${titleSplitWithBr[0]}<br>${trainNo} ${titleSplitWithBr[1]}`)
+    const titleSplitWithBr = $('.inner,.ek-onetrain-title-inner', document)
+        .html()
+        .split('<br>')
+    $('.inner,.ek-onetrain-title-inner', document).html(
+        `${titleSplitWithBr[0]}<br>${trainNo} ${titleSplitWithBr[1]}`
+    )
 }
-
