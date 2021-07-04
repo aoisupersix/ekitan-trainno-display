02f796eca205de46661d371708016109e8db1296
aoisupersix <aoisupersix@gmail.com>
2021-07-04 23:06:34 +0900

    test: Jestによるユニットテストを整備

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/src/**/*.ts',
+    '!**/tests/**',
+    '!**/node_modules/**',
+  ],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleNameMapper: {
+    '^#/(.+)': '<rootDir>/src/$1',
+  },
+}
---
+++ src/tests/trainnameno_extractor.test.ts
@@ -0,0 +1,15 @@
+import { extractTrainNameNo } from '../model/trainnameno_extractor'
+
+describe('extractTrainNameNo()', () => {
+    describe('東北新幹線', () => {
+        test.each`
+            trainName     | trainNo    | trainNameNo
+            ${'はやぶさ'} | ${'8010B'} | ${'10'}
+        `(
+            '$trainName($trainNo)の号数は$trainNameNo',
+            ({ trainName, trainNo, trainNameNo }) => {
+                expect(extractTrainNameNo(trainName, trainNo)).toBe(trainNameNo)
+            }
+        )
+    })
+})
