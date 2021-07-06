0036a3b13cb0da82c76eb987a7d9a0ed4a01ac43
aoisupersix <aoisupersix@gmail.com>
2021-07-06 21:30:13 +0900

    refactor: 列車番号の取得と表示処理をメソッドに抽出した

--- src/train_contentscript.ts
+++ src/train_contentscript.ts
@@ -8,6 +8,43 @@ import { extractTrainNameNo } from './models/trainnameno_extractor'
 const pagerTrainInfos = getPagerTrainInformations(document.body)
 
 /**
+ * クエリパラメータから列車番号を取得します。
+ * @returns クエリパラメータから取得した列車番号。取得できなかった場合はnullを返します
+ */
+const getTrainNoFromQuery = (): string | null => {
+    const txQuery = location.search
+        .substring(1)
+        .split('&')
+        .map((val) => ({ name: val.split('=')[0], value: val.split('=')[1] }))
+        .find((val) => val.name === 'tx')
+
+    return txQuery?.value?.split('-')[2] ?? null
+}
+
+/**
+ * 引数に指定された列車番号から列車情報(列車番号,号数)をページに表示します。
+ * @param trainNo 列車番号
+ */
+const displayTrainInfo = (trainNo: string): void => {
+    const title = document.querySelector('.inner,.ek-onetrain-title-inner')
+    const [route, trainInfo] = title?.innerHTML?.split('<br>') ?? [null, null]
+    const [trainName, destination] = trainInfo?.split('　') ?? [null, null]
+    const trainNameNo = extractTrainNameNo(trainName ?? '', trainNo)
+    const trainNameNoString = trainNameNo !== null ? `${trainNameNo}号` : ''
+
+    if (
+        trainNo !== null &&
+        title !== null &&
+        route !== null &&
+        trainName !== null &&
+        destination !== null
+    ) {
+        // eslint-disable-next-line no-irregular-whitespace
+        title.innerHTML = `${route}<br>${trainNo} ${trainName}${trainNameNoString}　${destination}`
+    }
+}
+
+/**
  * 発車時刻から列車情報を取得して更新があった場合に列車番号を更新します。
  */
 const updateTrainNoIfPageChanged = (): void => {
@@ -40,27 +77,7 @@ if (oneTrainElement !== null && pagerTrainInfos !== null) {
     })
 }
 
-const txQuery = location.search
-    .substring(1)
-    .split('&')
-    .map((val) => ({ name: val.split('=')[0], value: val.split('=')[1] }))
-    .find((val) => val.name === 'tx')
-
-if (txQuery !== undefined) {
-    const trainNo = txQuery.value.split('-')[2]
-    const title = document.querySelector('.inner,.ek-onetrain-title-inner')
-    const [route, trainInfo] = title?.innerHTML?.split('<br>') ?? [null, null]
-    const [trainName, destination] = trainInfo?.split('　') ?? [null, null]
-    const trainNameNo = extractTrainNameNo(trainName ?? '', trainNo)
-    const trainNameNoString = trainNameNo !== null ? `${trainNameNo}号` : ''
-
-    if (
-        title !== null &&
-        route !== null &&
-        trainName !== null &&
-        destination !== null
-    ) {
-        // eslint-disable-next-line no-irregular-whitespace
-        title.innerHTML = `${route}<br>${trainNo} ${trainName}${trainNameNoString}　${destination}`
-    }
+const trainNo = getTrainNoFromQuery()
+if (trainNo !== null) {
+    displayTrainInfo(trainNo)
 }
