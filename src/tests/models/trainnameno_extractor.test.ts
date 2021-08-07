c33e4e2512e005281f436e554e06414632638f44
aoisupersix <aoisupersix@gmail.com>
2021-08-07 18:44:29 +0900

    refactor: extractTrainNameNoのテスト名とテストを実行する関数を抜き出した

--- src/tests/models/trainnameno_extractor.test.ts
+++ src/tests/models/trainnameno_extractor.test.ts
@@ -1,15 +1,16 @@
 import { extractTrainNameNo } from '../../models/trainnameno_extractor'
 
+const testExtractTrainNameNoName = '$trainName($trainNo)の号数は$trainNameNo'
+
+const testExtractTrainNameNo = ({ trainName, trainNo, trainNameNo }) => {
+    expect(extractTrainNameNo(trainName, trainNo)).toBe(trainNameNo)
+}
+
 describe('extractTrainNameNo()', () => {
     describe('東北新幹線', () => {
         test.each`
             trainName     | trainNo    | trainNameNo
             ${'はやぶさ'} | ${'8010B'} | ${'10'}
-        `(
-            '$trainName($trainNo)の号数は$trainNameNo',
-            ({ trainName, trainNo, trainNameNo }) => {
-                expect(extractTrainNameNo(trainName, trainNo)).toBe(trainNameNo)
-            }
-        )
+        `(testExtractTrainNameNoName, testExtractTrainNameNo)
     })
 })
